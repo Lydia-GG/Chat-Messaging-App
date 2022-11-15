305754ae871d59d724a69e256932766aa107ac6a
Lydia Samir <L-Samir@hotmail.com>
2022-11-15 23:32:57 +0100

    added readme and user model

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -23,3 +23,15 @@ export const register = async (req, res, next) => {
     next(error);
   }
 };
+
+// Requiring ObjectId from mongoose npm package
+const ObjectId = require('mongoose').Types.ObjectId;
+
+// Validator function
+function isValidObjectId(id) {
+  if (ObjectId.isValid(id)) {
+    if (String(new ObjectId(id)) === id) return true;
+    return false;
+  }
+  return false;
+}
